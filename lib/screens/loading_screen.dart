06bf80b866b245b0589b7a52d635763ec315290b
roktim32 <roktimsenapoty2@gmail.com>
2021-07-03 02:49:58 +0530

    API added

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+// import 'dart:html';
+
 import 'package:climac/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,8 +23,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  //  api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a24cffa1713095d4afd649dda636114d
+  // http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a24cffa1713095d4afd649dda636114d
+  void getData() async {
+    final apiKey = "a24cffa1713095d4afd649dda636114d";
+    final requestUrl =
+        "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${apiKey}";
+
+    final http.Response respone = await http.get(Uri.parse(requestUrl));
+
+    if (respone.statusCode == 200) {
+      String data = respone.body;
+      print(data);
+    } else {
+      print(respone.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
